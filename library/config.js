13c379e2b702c073e1d63101561446eba63c63d7
Raj2710 <nagarajansai2727@gmail.com>
2021-07-07 13:55:11 +0530

    Commited

---
+++ library/config.js
@@ -0,0 +1,7 @@
+const sender="nagarajansai2727@gmail.com"
+const pwd = "raj2710@g"
+const CLIENT_ID="1094693455304-qlqoq90kafh134du91npemev5lfcqvk0.apps.googleusercontent.com"
+const CLIENT_SECRET="seMIWuJe1H2MMU0ygnQaMM85"
+const REDIRECT_URI="https://developers.google.com/oauthplayground"
+const REFRESH_TOKEN="1//04-9eDhhK-XN5CgYIARAAGAQSNwF-L9Irq973wxcsGFzCUNVYmEMQmS5n37-XnRauVQHDROiz3rTJfEArxx8OceMZhPbZoJTrlWQ"
+module.exports={sender,pwd,CLIENT_ID,CLIENT_SECRET,REDIRECT_URI,REFRESH_TOKEN};
