30a9b4191a5b0348a8c0d060e7076a031c2fc371
Raj2710 <nagarajansai2727@gmail.com>
2021-07-22 19:38:14 +0530

    Url updated

--- routes/users.js
+++ routes/users.js
@@ -220,7 +220,7 @@ router.post('/reset-password',async(req,res)=>{
         html:`<h2>Hello ${user.firstname}</h2>
         <p>We've recieved a request to reset the password for your account associated with your email.
         You can reset your password by clicking the link below</p>
-        <a href=https://urr.herokuapp.com/user/update-password/${token}> Reset Password</a>
+        <a href=https://urlshortneer.herokuapp.com/user/update-password/${token}> Reset Password</a>
         <p><b>Note:</b>The link expires 15 minutes from now</p>
         </div>`
       })
