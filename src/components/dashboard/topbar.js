7fadc00ac98e2deb2f8417777f702d020afe6682
Raj2710 <nagarajansai2727@gmail.com>
2021-07-25 14:18:24 +0530

    final commit

--- src/components/dashboard/topbar.js
+++ src/components/dashboard/topbar.js
@@ -25,7 +25,7 @@ export default function Topbar(props){
             <div className="top-right-elements">
                 <button className="create-button" onClick={handleEvent}>Create</button>
                 <i className="fas fa-user fa-2x" style={{color:"white"}}></i><h4 style={{color:"white"}}>{props.firstname}</h4>
-                <button className="btn btn-danger" onClick={handleLogout}>Logout</button>
+                <button className="btn btn-danger" onClick={handleLogout}><i className="fas fa-sign-out-alt"></i> Logout</button>
             </div>
         </div>
      </div>  
